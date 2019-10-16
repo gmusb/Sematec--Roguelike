b82b31f179f180b140b6edf617ee5040feb51b9d
GMusb <musbmg@gmail.com>
2019-10-15 22:04:24 -0300

    Criando o repositório certo desta vez

---
+++ Assets/Scripts/GeradorDeNivel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeNivel : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Start()
+    {
+        int rand = Random.Range(0, objects.Length);
+        Instantiate(objects[rand], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody rig;
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0.0f, vAxis) * speed; //* Time.deltaTime;//Ativa a velocidade por segundo
+
+        //rig.MovePosition(transform.position + movement);
+        rig.velocity = movement;
+    }
+}
